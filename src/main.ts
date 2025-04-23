4af628945bd9e0c5e837e5fdc9a647aa120af3f1
OisinForde05 <git config --global user.email g00434079@atu.ie>
2025-04-23 13:48:52 +0000

    Fixed majority of bugs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
-import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+ import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+ 
+ import { routes } from './app/app.routes';
+ import { AppComponent } from './app/app.component';
+ import { DetailsPage } from './app/details/details.page';
+import { ErrorHandler, importProvidersFrom } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage-angular';
+ 
+ bootstrapApplication(AppComponent, {
+   providers: [
+     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+     provideIonicAngular(),
+     provideRouter(routes, withPreloading(PreloadAllModules)),
+     provideRouter(routes, withPreloading(PreloadAllModules)),  // Preload modules
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err));
+     importProvidersFrom(IonicStorageModule.forRoot()),
+     {provide: ErrorHandler },
+   ],
+ });
